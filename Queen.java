c99bf566bfb9c9053a2ab61ece825a91d47628cb
Leodore <lemuellly@gmail.com>
2015-11-27 20:28:20 +0800

    Queen.java

---
+++ Queen.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class Queen {
+	private static int[] queen;
+	private static int queenNumber;
+	private static int rowNum = 1;
+	
+	public static void main(String[] args) {
+		System.out.println("Enter the number of queens and press Enter:");
+		
+		Scanner in = new Scanner(System.in);
+		queenNumber = in.nextInt();
+		in.close();
+		
+		System.out.println("The results are:");
+		
+		queen = new int[queenNumber];
+		queenResult(queenNumber);
+	}
+	
+	public static void queenResult(int number) {
+		
+		label:
+		while (rowNum >= 0) {			
+			if (queen[rowNum] < number)
+				queen[rowNum]++;
+			else {
+				rowNum--;
+				continue;
+			}
+			
+			while (queen[rowNum] < number) {				
+				if  (!canBePlaced(rowNum)) {
+					queen[rowNum]++;
+					continue;
+				}
+				
+				if (queen[rowNum] < number) {
+					if (rowNum == number - 1) {
+						for (int i = 0; i < number; i++) 
+							System.out.printf("%d ", queen[i] + 1);
+						
+						System.out.println();
+						
+						queen[rowNum]++;
+						continue;
+					}
+					else {
+						rowNum++;
+						queen[rowNum] = 0;
+						continue;
+					}
+				}
+				else {
+					rowNum--;
+					continue label;
+					}				
+			}
+		}
+	}
+	
+	public static boolean canBePlaced(int row) {
+		int i = 0;
+		while (i < row) {
+			if (queen[i] == queen[row] || Math.abs(row - i) == Math.abs(queen[row] - queen[i]))
+				return false;
+			
+			i++;
+			}
+		
+		return true;		
+	}
+}
