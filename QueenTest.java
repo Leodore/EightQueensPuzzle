63ce2d724814a5d8984342569b88a09a41e22c22
leodore <lemuellly@gmail.com>
2015-11-29 10:06:27 +0800

    Update QueenTest.java

--- QueenTest.java
+++ QueenTest.java
@@ -1,9 +1,11 @@
 public class QueenTest {
   public static void main(String[] args) {
-  	System.out.println("Enter the number of queens and press Enter:");
-		Scanner input = new Scanner(System.in);
-		int number = input.nextInt();
-		input.close();
+  		System.out.println("Enter the number of queens and press Enter:");
+  		
+		Scanner in = new Scanner(System.in);
+		int number = in.nextInt();
+		in.close();
+		
 		System.out.println("The results are:");
 		Queen.queenResult(number);
   	}
